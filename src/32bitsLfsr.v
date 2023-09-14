86df01cae497c0e65a77bd4febd96ea9a5c928be
odinx123 <68193880+odinx123@users.noreply.github.com>
2023-09-14 10:23:33 +0800

    Update 32bitsLfsr.v

--- src/32bitsLfsr.v
+++ src/32bitsLfsr.v
@@ -3,7 +3,27 @@ module top_module(
     input reset,    // Active-high synchronous reset to 32'h1
     output [31:0] q
 ); 
+
+    reg [31:0] next_q;
     
+    always @(*) begin
+        next_q = q[31:1];  // 右移一位
+        next_q[31] = q[0];  // q[0] ^ 0化簡
+        next_q[21] = q[22] ^ q[0];
+        next_q[1] = q[2] ^ q[0];
+        next_q[0] = q[1] ^ q[0];
+    end
+
+    always @(posedge clk) begin
+        if (reset) begin
+            q <= 32'd1;
+        end
+        else begin
+            q <= next_q;
+        end
+    end
+
+    /*
     always @(posedge clk) begin
         if (reset) begin
             q <= 32'd1;
@@ -17,5 +37,6 @@ module top_module(
             q[0] <= q[1] ^ q[0];
         end
     end
+    */
 
 endmodule
